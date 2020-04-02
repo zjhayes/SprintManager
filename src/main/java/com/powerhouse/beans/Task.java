ff7ee72b3e8cfa9ad1dc06ff991db12b3e7d0e9f
Jon Bos <bos.jon@gmail.com>
2020-04-02 17:59:17 -0500

    adds task model

--- src/main/java/com/powerhouse/beans/Task.java
+++ src/main/java/com/powerhouse/beans/Task.java
@@ -1,5 +1,16 @@
 package com.powerhouse.beans;
 
-public class Task {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+	private String subject;
+	private String description;
+	private String assignedUser;
+	private String currentStep;
+	private int storyPoints;
 }
