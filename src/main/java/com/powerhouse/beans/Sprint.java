40e7594ee648112fb44908c6dfa244581ae3df61
Jon Bos <bos.jon@gmail.com>
2020-04-02 18:46:29 -0500

    Adds annotations

--- src/main/java/com/powerhouse/beans/Sprint.java
+++ src/main/java/com/powerhouse/beans/Sprint.java
@@ -4,17 +4,38 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.springframework.context.annotation.Bean;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Entity
 @Data
 @AllArgsConstructor
 public class Sprint {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	private String name;
 	private String description;
 	private Date startDate;
 	private Date endDate;
 	private List<String> priorities;
+<<<<<<< Updated upstream
+=======
+	private List<String> workflows;
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "sprint", cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+>>>>>>> Stashed changes
 	private List<Task> tasks;
 
 	public Sprint() {
--- src/main/java/com/powerhouse/beans/Task.java
+++ src/main/java/com/powerhouse/beans/Task.java
@@ -1,5 +1,36 @@
 package com.powerhouse.beans;
 
+<<<<<<< Updated upstream
 public class Task {
 
+=======
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String subject;
+	private String description;
+	private String assignedUser;
+	// TODO - this will be a complicated join when custom priority schemes are implemented
+	private String currentStep;
+	private int storyPoints;
+	@ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH}, fetch = FetchType.EAGER)
+	private Sprint sprint;
+>>>>>>> Stashed changes
 }
