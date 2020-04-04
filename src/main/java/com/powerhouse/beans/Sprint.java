188c9b0ee962bd6c53e4456719be04114f3a4f95
Jon Bos <bos.jon@gmail.com>
2020-04-04 09:51:56 -0500

    refactor: add base entities to encapsulate name and id

--- src/main/java/com/powerhouse/beans/Sprint.java
+++ src/main/java/com/powerhouse/beans/Sprint.java
@@ -3,39 +3,26 @@ package com.powerhouse.beans;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
-@AllArgsConstructor
-public class Sprint {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String name;
+public class Sprint extends NamedEntity {
 	private String description;
 	private Date startDate;
 	private Date endDate;
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "sprint", cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "sprint", cascade = { CascadeType.MERGE, CascadeType.REMOVE })
 	private List<Task> tasks;
 
 	public Sprint() {
-		name = "";
+		setName("");
 		description = "";
 		tasks = new ArrayList<Task>();
 	}
+
 }
--- src/main/java/com/powerhouse/beans/Task.java
+++ src/main/java/com/powerhouse/beans/Task.java
@@ -3,9 +3,6 @@ package com.powerhouse.beans;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
@@ -16,15 +13,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Task {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+public class Task extends BaseEntity {
+
 	private String subject;
 	private String description;
 	private String assignedUser;
 	private String priority;
-	
+
 	// Starting simple
 	private boolean completed;
 
