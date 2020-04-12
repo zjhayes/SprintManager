39619e0184a17887e3a6b1060d2515364e3e9021
Jon Bos <bos.jon@gmail.com>
2020-04-12 13:03:59 -0500

    Update SecurityConfig.java

--- src/main/java/com/powerhouse/sprints/auth/config/SecurityConfig.java
+++ src/main/java/com/powerhouse/sprints/auth/config/SecurityConfig.java
@@ -14,7 +14,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// @formatter:off
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		//TODO
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("austin")
+		.password(bCryptPasswordEncoder().encode("austin"))
+		.roles("ROLES_USER")
+		.and()
+		.withUser("zach")
+		.password(bCryptPasswordEncoder().encode("zach"))
+		.roles("ROLES_USER")
+		.and()
+		.withUser("jon")
+		.password(bCryptPasswordEncoder().encode("jon"))
+		.roles("ROLES_USER");
+	}
+	// @formatter:on
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
