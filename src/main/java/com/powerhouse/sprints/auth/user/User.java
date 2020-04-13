b807a4b32c443c08bfb44da10916e01a2529d30d
Jon Bos <bos.jon@gmail.com>
2020-04-13 10:57:16 -0500

    adds user + role entities

---
+++ src/main/java/com/powerhouse/sprints/auth/user/User.java
@@ -0,0 +1,44 @@
+package com.powerhouse.sprints.auth.user;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.data.annotation.Transient;
+
+import com.powerhouse.sprints.model.BaseEntity;
+
+import lombok.Data;
+
+@Data
+public class User extends BaseEntity {
+	
+	@Column(name = "email", nullable = false, unique = true)
+	@Email(message = "Please provide a valid e-mail")
+	@NotEmpty(message = "Please provide an e-mail")
+	private String email;
+	
+	@Column(name = "password")
+	@Transient
+	private String password;
+	
+	@Column(name = "first_name")
+	@NotEmpty(message = "Please provide your first name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	@NotEmpty(message = "Please provide your last name")
+	private String lastName;
+	
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	@Column(name = "confirmation_token")
+	private String confirmationToken;
+
+	@ManyToMany
+	private Set<Role> roles;
+}
