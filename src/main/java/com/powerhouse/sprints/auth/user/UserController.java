089d435a9822e5f8836d267ce9467a20289ce0dd
Jon Bos <bos.jon@gmail.com>
2020-04-14 10:15:21 -0500

    Adds registration contrroller and html

--- src/main/java/com/powerhouse/sprints/auth/user/UserController.java
+++ src/main/java/com/powerhouse/sprints/auth/user/UserController.java
@@ -1,13 +1,37 @@
 package com.powerhouse.sprints.auth.user;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private UserService userService;
+	private EmailService emailService;
+
+	@Autowired
+	public UserController(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService,
+			EmailService emailService) {
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.userService = userService;
+		this.emailService = emailService;
+	}
 
 	@GetMapping("/")
 	public String welcome(Model model) {
@@ -24,4 +48,58 @@ public class UserController {
 		model.addAttribute("loginError", true);
 		return "auth/login";
 	}
+
+	// create registration form
+	@GetMapping("/register")
+	public ModelAndView createRegistrationForm(ModelAndView modelAndView, User user) {
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("auth/register");
+		return modelAndView;
+	}
+
+	// process registration form
+	@PostMapping("/register")
+	public ModelAndView processRegistrationForm(ModelAndView modelAndView, @Valid User user,
+			BindingResult bindingResult, HttpServletRequest request) {
+		// Lookup user in database by e-mail
+		User userExists = userService.findByEmail(user.getEmail());
+
+		if (userExists != null) {
+			modelAndView.addObject("alreadyRegisteredMessage",
+					"Oops!  There is already a user registered with the email provided.");
+			modelAndView.setViewName("auth/register");
+			bindingResult.reject("email");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("auth/register");
+		} else { // new user so we create user and send confirmation e-mail
+
+			// Disable user until they click on confirmation link in email
+			user.setEnabled(false);
+
+			// Generate random 36-character string token for confirmation link
+			user.setConfirmationToken(UUID.randomUUID().toString());
+
+			userService.saveUser(user);
+
+			String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+
+			SimpleMailMessage registrationEmail = new SimpleMailMessage();
+			registrationEmail.setFrom("noreply@CIS175Sprints@gmail.com");
+			registrationEmail.setTo(user.getEmail());
+			registrationEmail.setSubject("Registration Confirmation");
+			registrationEmail.setText("To confirm your e-mail address, please click the link below:\n" + appUrl
+					+ "/confirm?token=" + user.getConfirmationToken());
+			registrationEmail.setFrom("noreply@domain.com");
+
+			emailService.sendEmail(registrationEmail);
+
+			modelAndView.addObject("confirmationMessage", "A confirmation e-mail has been sent to " + user.getEmail());
+			modelAndView.setViewName("auth/register");
+		}
+
+		return modelAndView;
+
+	}
+
 }
