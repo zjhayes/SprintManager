e61cdd488400d41714991282121cd172d9604329
Jon Bos <bos.jon@gmail.com>
2020-04-14 13:07:37 -0500

    Adds confirmation controller and html

--- src/main/java/com/powerhouse/sprints/auth/user/UserController.java
+++ src/main/java/com/powerhouse/sprints/auth/user/UserController.java
@@ -1,8 +1,6 @@
 package com.powerhouse.sprints.auth.user;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +15,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
 
 @Controller
 public class UserController {
@@ -100,6 +103,56 @@ public class UserController {
 
 		return modelAndView;
 
+	}
+
+	@GetMapping("/confirm")
+	public ModelAndView getConfirmForm(ModelAndView modelAndView, @RequestParam("token") String token) {
+		User user = userService.findByConfirmationToken(token);
+
+		if (user == null) { // No token found in DB
+			modelAndView.addObject("invalidToken", "Oops!  This is an invalid confirmation link.");
+		} else { // Token found
+			modelAndView.addObject("confirmationToken", user.getConfirmationToken());
+		}
+		modelAndView.setViewName("auth/confirm");
+		return modelAndView;
+	}
+
+	// Process confirmation link
+	@PostMapping("/confirm")
+	public ModelAndView confirmRegistration(ModelAndView modelAndView, BindingResult bindingResult,
+			@RequestParam Map<String, String> requestParams, RedirectAttributes redir) {
+
+		modelAndView.setViewName("auth/confirm");
+
+		Zxcvbn passwordCheck = new Zxcvbn();
+
+		Strength strength = passwordCheck.measure(requestParams.get("password"));
+
+		if (strength.getScore() < 2) {
+			bindingResult.reject("password");
+
+			redir.addFlashAttribute("errorMessage", "Your password is too weak.  Choose a stronger one.");
+
+			modelAndView.setViewName("redirect:confirm?token=" + requestParams.get("token"));
+			System.out.println(requestParams.get("token"));
+			return modelAndView;
+		}
+
+		// Find the user associated with the reset token
+		User user = userService.findByConfirmationToken(requestParams.get("token"));
+
+		// Set new password
+		user.setPassword(bCryptPasswordEncoder.encode(requestParams.get("password")));
+
+		// Set user to enabled
+		user.setEnabled(true);
+
+		// Save user
+		userService.saveUser(user);
+
+		modelAndView.addObject("successMessage", "Your password has been set!");
+		return modelAndView;
 	}
 
 }
