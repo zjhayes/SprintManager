836d5c6aca112e38c93c08368514afa3ae9cd33a
Jon Bos <bos.jon@gmail.com>
2020-04-14 10:11:10 -0500

    Annotate User and Role

--- src/main/java/com/powerhouse/sprints/auth/user/Role.java
+++ src/main/java/com/powerhouse/sprints/auth/user/Role.java
@@ -3,21 +3,26 @@ package com.powerhouse.sprints.auth.user;
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToMany;
 
 import com.powerhouse.sprints.model.BaseEntity;
+import com.powerhouse.sprints.model.NamedEntity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Role extends BaseEntity {
 
-	@Column(name = "role", nullable = false)
-	@Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
 	private RoleEnum role;
 
 	@ManyToMany(mappedBy = "roles")
