8fdfb0f4644fac81052593248f0bc975911ffd8c
Jon Bos <bos.jon@icloud.com>
2020-04-21 15:13:26 -0500

    Adds routing + dashboard page

---
+++ src/main/java/com/powerhouse/sprints/auth/user/AdminController.java
@@ -0,0 +1,15 @@
+package com.powerhouse.sprints.auth.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+	@GetMapping("/admin")
+	public ModelAndView viewAdminDashboard(ModelAndView modelAndView) {
+		modelAndView.setViewName("admin/dashboard");
+		return modelAndView;
+	}
+}
