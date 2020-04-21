58f4bb6c1fe3d6dc40e7f0dcded7d1e7460a1434
Jon Bos <bos.jon@icloud.com>
2020-04-21 14:19:05 -0500

    Add user role to users at save

--- src/main/java/com/powerhouse/sprints/auth/service/UserService.java
+++ src/main/java/com/powerhouse/sprints/auth/service/UserService.java
@@ -1,9 +1,14 @@
 package com.powerhouse.sprints.auth.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.powerhouse.sprints.auth.user.Role;
+import com.powerhouse.sprints.auth.user.RoleEnum;
 import com.powerhouse.sprints.auth.user.RoleRepository;
 import com.powerhouse.sprints.auth.user.User;
 import com.powerhouse.sprints.auth.user.UserRepository;
@@ -26,6 +31,7 @@ public class UserService {
 	}
 
 	public void saveUser(User user) {
+		user.setRoles(new HashSet<Role>(Arrays.asList(roleRepository.findOneByRole(RoleEnum.ROLES_USER))));
 		userRepository.save(user);
 	}
 }
