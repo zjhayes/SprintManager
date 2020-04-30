aea8e4bb0d70028e69cacdad0e6458f51db74084
Jon Bos <bos.jon@gmail.com>
2020-04-30 09:24:32 -0500

    Adds TaskService

---
+++ src/main/java/com/powerhouse/sprints/sprint/TaskService.java
@@ -0,0 +1,17 @@
+package com.powerhouse.sprints.sprint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	@PostAuthorize("returnObject.project.projectMembers.contains(authentication.principal.user)")
+	public Task findById(long id) {
+		return taskRepository.findById(id).orElse(null);
+	}
+}
